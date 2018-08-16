c1b392b7eb05acfd6878f846cfcd6cf1d0632f8e
billmath <billmath@microsoft.com>
2018-08-16 14:39:00 -0400

    updating

--- 4_CreateUser.ps1
+++ 4_CreateUser.ps1
@@ -2,12 +2,14 @@
 $Givenname = "Allie"
 $Surname = "McCray"
 $Displayname = "Allie McCray"
+$Name = "amccray"
 $Password = "Pass1w0rd"
-$Identity = "CN=Allie McCray,CN=Users,DC=contoso,DC=com"
+$Identity = "CN=ammccray,CN=Users,DC=contoso,DC=com"
+$SecureString = ConvertTo-SecureString $Password -AsPlainText -Force
 
 
 # Create the user
-New-ADUser -GivenName $Givenname -Surname $Surname -DisplayName $Displayname -AccountPassword $Password
+New-ADUser -Name $Name -GivenName $Givenname -Surname $Surname -DisplayName $Displayname -AccountPassword $SecureString
 
 # Set the password to never expire
-Set-ADUser -Identity $Identity -PasswordNeverExpires $true
+Set-ADUser -Identity $Identity -PasswordNeverExpires $true -ChangePasswordAtLogon $false -Enabled $true
